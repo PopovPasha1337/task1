8e3fe18b1bf6f14f588754416ead23db10b738a1
PopovPasha1337 <prostost2le1337@gmail.com>
2024-10-19 16:17:54 +0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+#1st program
+print(9**0.5*5)
+
+#2 program
+print(9.99>9.98)
+print(1000 != 1000.1)
+
+#3 program
+print (2*2+2)
+print(2*(2+2))
+
+#4 program
+num = float('123.456')
+print(int(123.456 * 10) % 10)
